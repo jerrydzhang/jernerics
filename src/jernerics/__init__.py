2162a661fbfa8552890ef4b6a9d97fa45696ca3b
jerrydzhang <jerry.zhang.d@gmail.com>
2026-03-25 15:39:09 -0400

    fix: remove incomplete paths object from public API (fixes #41)

--- src/jernerics/__init__.py
+++ src/jernerics/__init__.py
@@ -1,6 +1,5 @@
 from .config import merge_configs
 from .dag import DAG, Provenance, RunState, Task, TaskState, TaskStatus, task
-from .paths import paths
 
 __all__ = [
     "DAG",
@@ -10,6 +9,5 @@ __all__ = [
     "TaskState",
     "TaskStatus",
     "merge_configs",
-    "paths",
     "task",
 ]
